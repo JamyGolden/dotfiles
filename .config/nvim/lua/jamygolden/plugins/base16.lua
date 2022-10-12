5d0ee0c7620b1d56d4c5473cb3c1881cd7b42f79
Jamy Golden <jamy@jamygolden.com>
2022-10-12 21:36:00 +0200

    Fix vim base16 loading script
    Logic wasn't correct previously

--- .config/nvim/lua/jamygolden/plugins/base16.lua
+++ .config/nvim/lua/jamygolden/plugins/base16.lua
@@ -1,8 +1,9 @@
 local fn = vim.fn
 local cmd = vim.cmd
 local set_theme_path = "$HOME/.config/base16-project/set_theme.lua"
+local is_set_theme_file_readable = fn.filereadable(fn.expand(set_theme_path)) == 1 and true or false
 
-if fn.filereadable(fn.expand(set_theme_path)) then
+if is_set_theme_file_readable then
   cmd("let base16colorspace=256")
   cmd("source " .. set_theme_path)
 end
