f1749ec14ee9957f9f2099870ca4a0ce6489a936
Jamy Golden <jamy@jamygolden.com>
2022-10-14 00:48:09 +0200

    Add option to allow neotest to run commands with a cwd of the test
    Add neotest keymaps

--- .config/nvim/lua/jamygolden/plugins/neotest.lua
+++ .config/nvim/lua/jamygolden/plugins/neotest.lua
@@ -3,20 +3,28 @@ local neotestJest = require('neotest-jest')
 local keymaps = require 'jamygolden/utils/keymaps'
 local nmap = keymaps.nmap
 
+local getPath = function(str,sep)
+    sep = sep or '/'
+    return str:match("(.*"..sep..")")
+end
+
 neotest.setup({
   adapters = {
     neotestJest({
       jestCommand = "yarn test:unit --",
-      -- jestConfigFile = "custom.jest.config.ts",
-      -- env = { CI = true },
-      cwd = function()
-        return vim.fn.getcwd()
+      cwd = function(path)
+
+        return getPath(path)
       end,
+      dap = { justMyCode = false },
     }),
-  }
+    require("neotest-plenary"),
+  },
 })
 
 -----------------------------------------------------------------------
 -- Keymaps
 -----------------------------------------------------------------------
-nmap('<Leader>tt', ':lua require("neotest").run.run()<cr>')
+nmap('<leader>ti', '<cmd>lua require("neotest").run.run()<cr>')
+nmap('<leader>ts', '<cmd>lua require("neotest").run.stop()<cr>')
+nmap('<leader>tp', '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<cr>')
