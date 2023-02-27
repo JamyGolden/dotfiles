75816df020464921667e20c7cda202f07805097a
Jamy Golden <jamy@jamygolden.com>
2023-02-27 19:40:09 +0100

    nvim: Add shortcuts to easily copy file path

--- .config/nvim/lua/jamygolden/keybindings.lua
+++ .config/nvim/lua/jamygolden/keybindings.lua
@@ -7,11 +7,21 @@ local vmap = keymaps.vmap
 
 vim.g.mapleader = ' '
 
+local set_path_to_global_clipboard_func = function()
+  return function(is_absolute)
+    local current_file_path = vim.fn.expand(is_absolute and "%" or "%:~:.")
+
+    vim.fn.setreg('+y', current_file_path)
+  end
+end
+
 map({ 'n', 'v' }, '<Leader>y', '\"+y', { silent = false })
 nmap('<Leader>Y', 'gg\"+yG', { silent = false })
 vmap('<Leader>p', '\"_dP')
 nmap('<Leader>qq', ':q<cr>')
 nmap('<Leader>qQ', ':qa<cr>')
+nmap('<Leader>cp', set_path_to_global_clipboard_func())
+nmap('<Leader>ca', set_path_to_global_clipboard_func(true))
 
 -- Close tmp windows (Like GitFugitive) with <Leader>q
 nmap('<Leader>q', function()
