b25777ff30eddabaa75f81a530f090c9d5153fe2
Jamy Golden <jamy@jamygolden.com>
2023-03-21 07:34:00 +0100

    vim: Use global base16 variable instead of executing vimscript

--- .config/nvim/lua/jamygolden/plugins/base16.lua
+++ .config/nvim/lua/jamygolden/plugins/base16.lua
@@ -1,9 +1,10 @@
 local fn = vim.fn
 local cmd = vim.cmd
+local g = vim.g
 local set_theme_path = "$HOME/.config/tinted-theming/set_theme.lua"
 local is_set_theme_file_readable = fn.filereadable(fn.expand(set_theme_path)) == 1 and true or false
 
 if is_set_theme_file_readable then
-  cmd("let base16colorspace=256")
+  g.base16colorspace = 256
   cmd("source " .. set_theme_path)
 end
