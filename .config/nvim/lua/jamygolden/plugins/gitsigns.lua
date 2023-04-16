880a42a7266e821c077ba6b92557ca134a9d3b52
Jamy Golden <jamy@jamygolden.com>
2023-04-16 20:55:59 +0200

    nvim: Add short git sha to git-blame line

--- .config/nvim/lua/jamygolden/plugins/gitsigns.lua
+++ .config/nvim/lua/jamygolden/plugins/gitsigns.lua
@@ -1,6 +1,7 @@
 local gitsigns = require('gitsigns')
 
 gitsigns.setup({
+  current_line_blame_formatter = '<author>, <abbrev_sha>, <author_time:%Y-%m-%d> - <summary>',
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
 
