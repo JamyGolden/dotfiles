9c07ec72ee670b84a0f5bd9513e6ce8817dedf51
Jamy Golden <jamy@jamygolden.com>
2023-08-15 16:42:41 +0200

    nvim: Add relative path to move function in neotree
    Re-add change which was initially added in 69948af4

--- .config/nvim/lua/jamygolden/plugins/neo-tree.lua
+++ .config/nvim/lua/jamygolden/plugins/neo-tree.lua
@@ -1,11 +1,11 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
   keys = {
     { "-", "<cmd>Neotree toggle<cr>", desc = "Toggle (NeoTree)" },
   },
@@ -19,5 +19,15 @@ return {
         enabled = true -- This will find and focus the file in the active buffer every
       },
     },
+    window = {
+      mappings = {
+        ["m"] = { -- takes text input for destination, also accepts the optional config.show_path option like "add".
+          "move",
+          config = {
+            show_path = "relative" -- "none", "relative", "absolute"
+          }
+        }
+      }
+    }
   }
 }
