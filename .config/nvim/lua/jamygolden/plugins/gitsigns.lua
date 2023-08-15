4d4059388cec16abcf3c84a719665f91cb280951
Jamy Golden <jamy@jamygolden.com>
2023-08-15 21:20:36 +0200

    nvim: Add leading `h` namespace for the outlier gitsigns keys

--- .config/nvim/lua/jamygolden/plugins/gitsigns.lua
+++ .config/nvim/lua/jamygolden/plugins/gitsigns.lua
@@ -33,10 +33,10 @@ return {
       map("n", "<leader>hR", gs.reset_buffer)
       map("n", "<leader>hp", gs.preview_hunk)
       map("n", "<leader>hb", function() gs.blame_line{full=true} end)
-      map("n", "<leader>tb", gs.toggle_current_line_blame)
+      map("n", "<leader>htb", gs.toggle_current_line_blame)
       map("n", "<leader>hd", gs.diffthis)
       map("n", "<leader>hD", function() gs.diffthis("~") end)
-      map("n", "<leader>td", gs.toggle_deleted)
+      map("n", "<leader>htd", gs.toggle_deleted)
 
       -- Text object
       map({"o", "x"}, "ih", ":<C-U>Gitsigns select_hunk<CR>")
