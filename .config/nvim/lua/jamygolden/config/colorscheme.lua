a208da3b0adbca0b66952853b9e4e9a053013ef6
Jamy Golden <jamy@jamygolden.com>
2023-12-20 14:05:51 +0100

    nvim: Add new script to ensure vim theme matches config settings

--- .config/nvim/lua/jamygolden/config/colorscheme.lua
+++ .config/nvim/lua/jamygolden/config/colorscheme.lua
@@ -1,10 +1,38 @@
-local fn = vim.fn
-local cmd = vim.cmd
-local g = vim.g
-local set_theme_path = "$XDG_CONFIG_HOME/tinted-theming/set_theme.lua"
-local is_set_theme_file_readable = fn.filereadable(fn.expand(set_theme_path)) == 1 and true or false
+local theme_script_path = vim.fn.expand("$XDG_CONFIG_HOME/tinted-theming/set_theme.lua")
+local is_set_theme_file_readable = vim.fn.filereadable(theme_script_path) == 1 and true or false
+local theme_name_path = vim.fn.expand("$XDG_CONFIG_HOME/tinted-theming/theme_name")
+
+local function read_file(path)
+  local file, err = io.open(path, "r")
+
+  if not file then
+    print("Error opening file: " .. err)
+
+    return nil
+  end
+
+  local content = file:read("*a")
+  file:close()
+
+  return content
+end
+
+local function handle_focus_gained()
+  local current_theme_name = vim.g.colors_name
+  local new_theme_name = "base16-" .. read_file(theme_name_path)
+
+  if current_theme_name ~= new_theme_name then
+    dofile(theme_script_path)
+  end
+end
 
 if is_set_theme_file_readable then
-  g.base16colorspace = 256
-  cmd("source " .. set_theme_path)
+  vim.o.termguicolors = true
+  vim.g.base16colorspace = 256
+
+  dofile(theme_script_path)
+
+  vim.api.nvim_create_autocmd("FocusGained", {
+    callback = handle_focus_gained,
+  })
 end
