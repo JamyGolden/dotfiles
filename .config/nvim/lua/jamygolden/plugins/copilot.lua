e07c5f3d7c0342834c606a3e8a6d7db4f832fcac
Jamy Golden <jamy@jamygolden.com>
2024-01-09 10:29:32 +0100

    nvim: Move copilot disable into conditional

--- .config/nvim/lua/jamygolden/plugins/copilot.lua
+++ .config/nvim/lua/jamygolden/plugins/copilot.lua
@@ -1,15 +1,15 @@
 return {
   "github/copilot.vim",
   config = function()
-    -- Force disable everywhere
-    vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-      pattern = '*',
-      command = "Copilot disable"
-    })
-
     -- Force enable for work projects if the env var is set
     local workDirectoryPath = os.getenv("WORK_DIRECTORY")
     if workDirectoryPath then
+      vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+        pattern = os.getenv("WORK_DIRECTORY") .. "/*",
+        command = "Copilot enable"
+      })
+    else
+      -- Force disable everywhere
       vim.api.nvim_create_autocmd({ 'BufEnter' }, {
         pattern = os.getenv("WORK_DIRECTORY") .. "/*",
         command = "Copilot enable"
