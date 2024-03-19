4ddf4c994ebf2e1d42f8132e261203b8d7dc5aac
Jamy Golden <code@jamygolden.com>
2024-03-19 22:21:55 +0100

    nvim: Remove unused keybindings

--- .config/nvim/lua/jamygolden/config/keymaps.lua
+++ .config/nvim/lua/jamygolden/config/keymaps.lua
@@ -14,17 +14,5 @@ map({ "n" }, "<leader>Y", "gg\"+yG", { silent = false })
 map({ "n" }, "<C-u>", "<C-u>zz")
 map({ "n" }, "<C-d>", "<C-d>zz")
 map({ "v" }, "<leader>p", "\"_dP")
-map({ "n" }, "<leader>qq", ":q<cr>")
-map({ "n" }, "<leader>qQ", ":qa<cr>")
 map({ "n" }, "<leader>cp", set_path_to_global_clipboard_func())
 map({ "n" }, "<leader>cP", set_path_to_global_clipboard_func(true))
-
--- Close tmp windows (Like GitFugitive) with <leader>q
-map({ "n" }, "<leader>q", function()
-  local path = vim.fn.expand("%")
-
-  if string.find(path, "/tmp/") ~= nil then
-    print("inside!", path)
-    vim.cmd("q")
-  end
-end)
