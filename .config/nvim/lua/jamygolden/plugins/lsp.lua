6731b723bf31927d2a518ff83335adba2c8a7dda
Jamy Golden <code@jamygolden.com>
2024-03-19 22:21:40 +0100

    nvim: Switch from nvim lsp for rust-analyzer to rustacean

--- .config/nvim/lua/jamygolden/plugins/lsp.lua
+++ .config/nvim/lua/jamygolden/plugins/lsp.lua
@@ -56,7 +56,6 @@ return {
 
       local servers = {
         "kotlin_language_server",
-        "rust_analyzer",
         "jsonls",
         "tsserver",
       }
@@ -68,31 +67,6 @@ return {
           on_attach = on_attach,
         })
       end
-
-      -- rust
-      lspconfig.rust_analyzer.setup {
-        capabilities = capabilities,
-        flags = lsp_flags,
-      on_attach = function(client, bufnr)
-        -- Only format if server supports it
-        if client.server_capabilities.documentFormattingProvider then
-            local group = vim.api.nvim_create_augroup("LspFormatting", {})
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = group,
-                buffer = bufnr,
-                callback = function()
-                    -- Using vim.lsp.buf.format instead of formatting_sync for better compatibility
-                    vim.lsp.buf.format({
-                        filter = function(client)
-                            return client.name == "rust_analyzer"
-                        end,
-                        bufnr = bufnr
-                    })
-                end,
-            })
-        end
-    end,
-      }
 
       -- lua
       lspconfig.lua_ls.setup {
---
+++ .config/nvim/lua/jamygolden/plugins/rustaceanvim.lua
@@ -0,0 +1,35 @@
+local code_action = function()
+  vim.cmd.RustLsp 'codeAction'
+end
+
+local hover_info = function()
+  vim.cmd.RustLsp { 'hover', 'actions' }
+end
+
+local explain_error = function()
+  vim.cmd.RustLsp 'explainError'
+end
+
+local go_to_parent_module  = function()
+  vim.cmd.RustLsp 'parentModule'
+end
+
+local render_diagnostics  = function()
+  vim.cmd.RustLsp 'renderDiagnostics'
+end
+
+local M = {
+  'mrcjkb/rustaceanvim',
+  version = '^4',
+  ft = { 'rust' },
+}
+
+M.keys = {
+  { "<leader>rg", code_action, desc = "Undo history" },
+  { "<leader>rK", hover_info, desc = "Hover information" },
+  { "<leader>ri", explain_error, desc = "Explain error" },
+  { "<leader>r6", go_to_parent_module, desc = "Go to parent module" },
+  { "<leader>rd", render_diagnostics, desc = "Render diagnostic" },
+}
+
+return M
