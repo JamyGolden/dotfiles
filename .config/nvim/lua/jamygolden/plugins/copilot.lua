023eeeaf4b566e9c9e111deb66a098a3939a2cbc
Jamy Golden <code@jamygolden.com>
2024-11-15 22:28:15 +0100

    nvim: Only init copilot for work

--- .config/nvim/lua/jamygolden/plugins/copilot.lua
+++ .config/nvim/lua/jamygolden/plugins/copilot.lua
@@ -2,10 +2,14 @@
 local workDirectoryPath = os.getenv("WORK_DIRECTORY") or ""
 local startMatch = string.find(vim.fn.getcwd(), workDirectoryPath)
 
-if startMatch then
-  return {
-    "github/copilot.vim",
-  }
-else
-  return {}
-end
+-- If there are issues, run `:Copilot setup`
+return {
+  "github/copilot.vim",
+  config = function()
+    if startMatch then
+      vim.cmd("Copilot enable");
+    else
+      vim.cmd("Copilot disable");
+    end
+  end
+}
